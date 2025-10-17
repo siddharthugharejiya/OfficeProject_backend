e323583f1387a0af54c570890600de5da9a33bf9
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2025-10-17 17:46:21 +0530

    .

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(cors({
-    origin: "*", // ✅ Allow all origins
+    origin: "https://www.prettywareceramikallp.com/", // ✅ Allow all origins
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
