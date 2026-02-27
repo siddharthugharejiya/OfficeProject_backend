840350d8558edeccb6892d25f950deb70228bc8b
Siddharth Ugharejiya <sidhdharthugharejiya8182@gmail.com>
2026-02-27 16:29:04 +0530

    .

--- app.js
+++ app.js
@@ -24,7 +24,13 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://prettywareceramikallp.com",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 // Use your Product routes
 app.use("/", ProductRoutes);
 
